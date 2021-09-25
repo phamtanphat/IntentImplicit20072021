f084773d9fa1c011092ffb4163d1c9211599953e
phamtanphat <phatdroid94@gmail.com>
2021-09-25 19:30:26 +0700

    camera

--- app/src/main/java/com/example/intentimplicit20072021/MainActivity.java
+++ app/src/main/java/com/example/intentimplicit20072021/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.intentimplicit20072021;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.ActivityResultRegistry;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 
 import com.example.intentimplicit20072021.databinding.ActivityMainBinding;
@@ -15,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding mMainBinding;
     int REQUEST_CODE_CAMERA = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,16 +36,38 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (ActivityCompat.checkSelfPermission(
-                        MainActivity.this ,
-                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                        MainActivity.this,
+                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(
                             MainActivity.this,
                             new String[]{Manifest.permission.CAMERA},
                             REQUEST_CODE_CAMERA
                     );
+                }else{
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    cameraLauncher.launch(intent);
                 }
             }
         });
-
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_CAMERA){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraLauncher.launch(intent);
+            }
+        }
+    }
+
+    ActivityResultLauncher<Intent> cameraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result != null){
+                mMainBinding.imageview.setImageBitmap((Bitmap) result.getData().getExtras().get("data"));
+            }
+        }
+    });
 }
