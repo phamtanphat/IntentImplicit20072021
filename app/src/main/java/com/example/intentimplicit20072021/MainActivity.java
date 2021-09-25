6f5fa9d0e078db78303bcafd13380aab907f6f37
phamtanphat <phatdroid94@gmail.com>
2021-09-25 18:50:01 +0700

    request permission

--- app/src/main/java/com/example/intentimplicit20072021/MainActivity.java
+++ app/src/main/java/com/example/intentimplicit20072021/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.intentimplicit20072021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.intentimplicit20072021.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mMainBinding;
+    int REQUEST_CODE_CAMERA = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mMainBinding.getRoot());
+
+        mMainBinding.buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(
+                        MainActivity.this ,
+                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            REQUEST_CODE_CAMERA
+                    );
+                }
+            }
+        });
+
     }
 }
